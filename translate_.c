b4ad6f16990b1473d664b0dc21e983cf0f7ea7cc
root <3626@holbertonschool.com>
2021-12-17 10:14:17 -0800

    test

--- translate_.c
+++ translate_.c
@@ -17,13 +17,13 @@ int translate_(int ac, char *av[])
 	var.stack_len = 0;
 	if (ac != 2)
 	{
-		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
+		fprintf(stdout, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(av[1], "r");
 	if (fp == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", av[1]);
+		fprintf(stdout, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&ptr, &a, fp) != -1)
