46d0390d2195007e8c8e54959bc4eca68e0c10dc
root <3626@holbertonschool.com>
2021-12-17 14:05:29 -0800

    test

--- three_swap.c
+++ three_swap.c
@@ -1,11 +1,23 @@
 #include "monty.h"
 /**
- *
- *
- *
- *
- *
- *
- *
+ *three_swap - swap top two elments of the stack
+ *@stack: stack
+ *@linenum: line number
  */
+void three_swap(stack_t **stack, unsigned int linenum)
+{
+	stack_t *a;
 
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", linenum);
+		free(*stack);
+		exit(EXIT_FAILURE);
+	}
+a = (*stack)->next;
+(*stack)->next = a->next;
+a->next = (*stack);
+(*stack)->prev = a;
+a->prev = NULL;
+*stack = a;
+}
