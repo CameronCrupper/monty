83150add73517d73c39d51acd7e3e192f749f8af
root <3626@holbertonschool.com>
2021-12-17 13:17:38 -0800

    test

--- zero_pall.c
+++ zero_pall.c
@@ -1,20 +1,14 @@
 #include "monty.h"
 /**
- *pall_ - print value on stack
+ *zero_pall - print value on stack
  *@stack: stack
  *@linenum: line number
  */
-void pall_(stack_t **stack, unsigned int linenum)
+void zero_pall(stack_t **stack, unsigned int linenum)
 {
-	int a = 0;
-	stack_t *NN = *stack;
-
+	stack_t *current;
 	(void)linenum;
 
-	while (NN != NULL)
-	{
-		printf("%d\n", NN->n);
-		NN = NN->next;
-		a++;
-	}
+	for (current = *stack; current != NULL; current = current->next)
+	printf("%d\n", current->n);
 }
